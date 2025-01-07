3a30d569f975017bbb4a53d160a3325a2f70a143
mr-amirfazel <amirfazel45@gmail.com>
2025-01-08 00:22:17 +0330

    handling abbreviations in normalization

--- Preprocess/normalize.py
+++ Preprocess/normalize.py
@@ -5,13 +5,30 @@ from hazm import Normalizer as HNormalizer
 
 class Normalizer:
     def __init__(self):
-        pass
+        self.abbreviations = {
+            "ا.م.": "استاندار محترم",
+            "د.ک.": "دکتری",
+            "ص.ن.": "صبحانه ناهار",
+            "پ.ن.": "پاورقی نویسنده",
+            "ق.ا.": "قانون اساسی",
+            "ه.ش.": "هجری شمسی",
+            "ه.ق.": "هجری قمری",
+            "م.م.": "معاون مدیر",
+            "ت.ت.": "تحقیق و توسعه",
+            "ر.ک.": "رجوع کنید",
+            "خ.ه.": "خارج از حوزه",
+            "ن.م.": "نامعلوم",
+            "ب.ن.": "به نظر",
+            "ت.ش.": "تحقیق شده",
+            "م.ه.": "محل همایش"
+        }
 
     def apply(self, text):
         if text is not None:
             text = self.__remove_punctuations(text)
             text = self.__hazm_normalize(text)
             text = self.__correct_spacing(text)
+            text = self.__remove_abbreviations(text)
             return text
         else:
             raise Exception("No content entered.")
@@ -52,4 +69,9 @@ class Normalizer:
             text = text.replace(email, email_token)
 
         text = re.sub(r'[^\w\s‌]', '', text)
+        return text
+
+    def __remove_abbreviations(self, text):
+        for abbr, full_form in self.abbreviations.items():
+            text = text.replace(abbr, full_form)
         return text
